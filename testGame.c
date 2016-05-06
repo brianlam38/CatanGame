2cfda151e75fbf0374c03e86583501f87b468ced
brianlam38 <brian.lam38@gmail.com>
2016-05-06 11:48:15 +1000

    updating all files

--- testGame.c
+++ testGame.c
@@ -1,13 +1,42 @@
-<<<<<<< HEAD
-brian
-=======
 #include <stdio.h>
 #include <stdlib.h>
-#include "math.h"
-#include "assert.h"
-int main (int argc, char *argv[]){
- 
+#include <math.h>
+#include <time.h>
+#include <assert.h>
+#include "Game.h"
+
+#define PATH_LIMIT 150
+
+/* Function declarations
+int getCampus(Game g, path pathToVertex); // return the contents of the given vertex (ie campus code or // VACANT_VERTEX) 
+int getARC (Game g, path pathToEdge); // the contents of the given edge (ie ARC code or vacent ARC)
+int getDiscipline (Game g, int regionID);
+int getDiceValue (Game g, int regionID);
+*/
+
+void getCampus (void); 
+
+int main (int argc, char *argv[]) {
+
+assert (getCampus(Game g, pcath pathToVertex) == VACANT_VERTEX); // 0
+assert (getCampus(Game g, path pathToVertex) == CAMPUS_A);		// 1
+assert (getCampus(Game g, path pathToVertex) == CAMPUS_B);		// 2
+assert (getCampus(Game g, path pathToVertex) == CAMPUS_C);		// 3
+assert (getCampus(Game g, path pathToVertex) == GO8_A);         // 4
+assert (getCampus(Game g, path pathToVertex) == GO8_B); 		// 5
+assert (getCampus(Game g, path pathToVertex) == GO8_C); 		// 6
+
+return EXIT_SUCCESS;
+}
+
+void getCampus (void) {
 
 }
 
->>>>>>> c8a86e288995ae08dce12e21d2a0e5a660ab8e4b
+// Game is a TYPE. Variable = g
+// path is "probably" a TYPE. Variable = pathToVertex
+// (1) Write a few tests on the initial game state including at least one test for:
+// (2) Write a few tests to demonstrate how you might test throwDice and makeAction.
+
+
+
---
+++ testGame2.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <assert.h>
+
+#define PATH_LIMIT 150
+
+// Game is a TYPE. Variable = g
+// path is "probably" a TYPE. Variable = pathToVertex
+
+// (1) Write a few tests on the initial game state including at least one test for:
+// (2) Write a few tests to demonstrate how you might test throwDice and makeAction.
+
+/* Function declarations */
+int getCampus(Game g, path pathToVertex); // return the contents of the given vertex (ie campus code or // VACANT_VERTEX) 
+int getARC(Game g, path pathToEdge); // the contents of the given edge (ie ARC code or vacent ARC)
+int getDiscipline (Game g, int regionID);
+int getDiceValue (Game g, int regionID);
+
+/* ASSERTS */
+assert (getCampus(Game g, path pathToVertex) == VACANT_VERTEX); // 0
+assert (getCampus(Game g, path pathToVertex) == CAMPUS_A);		// 1
+assert (getCampus(Game g, path pathToVertex) == CAMPUS_B);		// 2
+assert (getCampus(Game g, path pathToVertex) == CAMPUS_C);		// 3
+assert (getCampus(Game g, path pathToVertex) == GO8_A);         // 4
+assert (getCampus(Game g, path pathToVertex) == GO8_B); 		// 5
+assert (getCampus(Game g, path pathToVertex) == GO8_C); 		// 6
+
+
+
+
+
+int main (int argc, char *argv[]){
+
+return EXIT_SUCCESS;
+}
+
+
+
+
+EXTRA INFO
+
+// Example assert from Doomsday
+// assert (dayOfWeek (THURSDAY,  FALSE,  4,  4) == THURSDAY);
+
+--> Game g
+// newGame (disciplines, dice);
+// Make a new game, given the disciplines produced by each region
+// and the value on the dice discs in each region.
+--> path pathToVertex
+// a path is a sequence of L=left R=right B=back steps
+// starting from the initial campus of player 1 / A at the top 
+// of the map facing inwards represented as a string of length 
+// PATH_LIMIT or less (including the terminating 0).  
+// a path can specify a vertex (the vertex at the end of the path)
+// and a path can specify an ARC (the last ARC in the path)
+// it is fine and legal for a path to not be the shortest path
+// to the destination, it can even contain loops if you like.
+// The length of a path (including the terminating 0) must never 
+// exceed PATH_LIMIT.  Every edge in the path must lie on the 		// PATH_LIMIT = 150
+// island, paths cannot include edges which are in the sea.
+
+CONTENTS OF A VERTEX MAY INCLUDE:
+#define VACANT_VERTEX 0  
+#define CAMPUS_A 1
+#define CAMPUS_B 2
+#define CAMPUS_C 3
+#define GO8_A 4
+#define GO8_B 5
+#define GO8_C 6
+
+
+
+
+
+
+
+
+
+
+
