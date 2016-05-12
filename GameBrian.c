b28d126661fccd2a5eb5528847b2cd972d8ba43e
brianlam38 <brian.lama38@gmail.com>
2016-05-12 15:26:06 +1000

    adding GameBrian.c

---
+++ GameBrian.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "game.h"
+
+/* POSSIBLE STRUCTS */
+// Struct for University Resources (to hold data about university resources i.e discplines + publications + IPs)
+
+// returns the content of the given vertex (campus code or VACANT_VERTEX)
+void getCampus (Game g, vertex v) {
+	int vertexContent = 0;
+}
